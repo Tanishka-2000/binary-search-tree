9954ff66e26111fa806d5fb58ea924b4f82bea67
Shashank Talwar <shashanktalwar81@gmail.com>
2022-09-01 22:15:21 +0530

    add insert function

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+// to display tree
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+
 class Node{
-    constructor(value){
-        this.value = value;
+    constructor(data){
+        this.data = data;
         this.left = null;
         this.right = null;
     }
@@ -20,6 +31,28 @@ class Tree {
         node.right = this.buildTree(arr, mid+1, end);
         return node;
     }
+
+    insert(value){
+        let node = this.root;
+        while(true){
+            if(node.data > value){
+                if(node.left) node = node.left;
+                else {
+                    node.left = new Node(value);
+                    return;
+                }
+            }else{
+                if(node.right) node = node.right;
+                else {
+                    node.right = new Node(value);
+                    return;
+                }
+            }
+        }
+    }
 }
+
 let tree = new Tree([3,3,56,12,78,54,90,92]);
-console.log(tree.root);
+prettyPrint(tree.root);
+tree.insert(76);
+prettyPrint(tree.root);
